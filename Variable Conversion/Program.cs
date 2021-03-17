7055acbd176d6c9df19d687625b036e98ea8368c
Z M Asif Jamil <80680790+FurySimo@users.noreply.github.com>
2021-03-17 10:14:10 +0600

    Add files via upload

---
+++ Variable Conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variable_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myint = 20;
+            double mydouble = 13.77;
+            bool myBool = true;
+            Console.WriteLine(Convert.ToString(myint));
+            Console.WriteLine(Convert.ToDouble(myint));
+            Console.WriteLine(Convert.ToInt32(mydouble));
+            Console.WriteLine(Convert.ToString(myBool));
+        }
+    }
+}
